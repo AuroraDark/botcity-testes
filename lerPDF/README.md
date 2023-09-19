0b801d3aa779a2646a5f4e851246581be79863be
AuroraDark <andressa.workti@gmail.com>
2023-09-19 16:41:32 -0300

    Ler PDF

---
+++ lerPDF/README.md
@@ -0,0 +1,4 @@
+## Ler PDF
+
+Extrai dados de todos os PDFs dentro de uma pasta e transforma em uma planilha excel.
+(O PDF possui um padrão)
--- lerPDF/bot.py
+++ lerPDF/bot.py
@@ -1,29 +1,35 @@
 from botcity.document_processing import *
 import pathlib
+import pandas as pd
 
 def lerPDF(arquivo):
     reader = PDFReader()
-    parser = reader.read_file(r'./docs/Contoso_INVOICE_Fabrikam_UK.pdf')
+    parser = reader.read_file(arquivo)
+    page_data = {}
 
     _date = parser.get_first_entry("Date:")
-    value = parser.read(_date, 1.4, -1.25, 3.15, 2.416667)
-    print('Date:' + value)
+    page_data["date"] = parser.read(_date, 1.4, -1.25, 3.15, 2.416667)
 
     _bill_to = parser.get_first_entry("Bill to:")
-    value = parser.read(_bill_to, 1.240741, -1.416667, 3.555556, 2.833333)
-    print('Bill to:' + value)
+    page_data["bill_to"] = parser.read(_bill_to, 1.240741, -1.416667, 3.555556, 2.833333)
 
     _contact = parser.get_first_entry("Contact:")
-    value = parser.read(_contact, 1.065789, -1.333333, 4, 3.166667)
-    print('Contact:' + value)
+    page_data["contact"] = parser.read(_contact, 1.065789, -1.333333, 4, 3.166667)
 
     _balance_due = parser.get_first_entry("Balance due:")
-    value = parser.read(_balance_due, 1.14, -1.071429, 1.266667, 2.214286)
-    print('Balance Due:' + value)
-   
+    page_data["balance_due"] = parser.read(_balance_due, 1.14, -1.071429, 1.266667, 2.214286)
 
-arquivos = pathlib.Path(r"C:\Users\dessa\projetos\botcity-teste\lerPDF\docs").glob("*.pdf")
-
-lerPDF()
+    return page_data
 
 
+arquivos = pathlib.Path(r"C:\Users\andre\PycharmProjects\botcity-testes\lerPDF\docs").glob("*.pdf")
+
+pdf_data = []
+for arquivo in arquivos:
+    pdf_data.append(lerPDF(arquivo))
+
+
+print(pdf_data)
+
+df = pd.DataFrame(pdf_data)
+df.to_excel(r"dados.xlsx", index=False)
